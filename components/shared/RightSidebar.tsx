1075c659ce05410dff4af1704a627e1c33361464
Karl George <karlgdev@gmail.com>
2023-09-25 14:21:28 +0200

    styling right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,7 +1,18 @@
 interface RightSidebarProps {}
 
 function RightSidebar({}: RightSidebarProps) {
-  return <div>RightSidebar</div>;
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-col flex-1 justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>
+          Suggested Communities
+        </h3>
+      </div>
+      <div className='flex flex-col flex-1 justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+      </div>
+    </section>
+  );
 }
 
 export default RightSidebar;
