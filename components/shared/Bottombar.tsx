cbf04d8b0dd5cd2a4c7ae302fbb271dec4de6565
Karl George <karlgdev@gmail.com>
2023-09-25 14:15:15 +0200

    Styling bottom bar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,7 +1,44 @@
+'use client';
+
+import { sidebarLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 interface BottombarProps {}
 
 function Bottombar({}: BottombarProps) {
-  return <div>Bottombar</div>;
+  const pathname = usePathname();
+
+  return (
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className='text-light-1 text-subtle-medium max-sm:hidden'>
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Bottombar;
