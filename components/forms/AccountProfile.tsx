735b61f385e48756ce5188ae43c092485024a175
Karl George <karlgdev@gmail.com>
2023-09-25 15:05:39 +0200

    Styling form inputs

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -104,11 +104,11 @@ function AccountProfile({ user, btnTitle }: AccountProfileProps) {
           control={form.control}
           name='name'
           render={({ field }) => (
-            <FormItem className='flex items-center gap-3 w-full'>
+            <FormItem className='flex gap-3 w-full flex-col'>
               <FormLabel className='text-base-semibold text-light-2'>
                 Name
               </FormLabel>
-              <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <FormControl>
                 <Input
                   type='text'
                   className='account-form_input no-focus'
@@ -123,11 +123,11 @@ function AccountProfile({ user, btnTitle }: AccountProfileProps) {
           control={form.control}
           name='username'
           render={({ field }) => (
-            <FormItem className='flex items-center gap-3 w-full'>
+            <FormItem className='flex gap-3 w-full flex-col'>
               <FormLabel className='text-base-semibold text-light-2'>
                 User Name
               </FormLabel>
-              <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <FormControl>
                 <Input
                   type='text'
                   className='account-form_input no-focus'
@@ -142,11 +142,11 @@ function AccountProfile({ user, btnTitle }: AccountProfileProps) {
           control={form.control}
           name='bio'
           render={({ field }) => (
-            <FormItem className='flex items-center gap-3 w-full'>
+            <FormItem className='flex gap-3 w-full flex-col'>
               <FormLabel className='text-base-semibold text-light-2'>
                 Bio
               </FormLabel>
-              <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <FormControl>
                 <Textarea
                   rows={10}
                   className='account-form_input no-focus'
@@ -156,7 +156,9 @@ function AccountProfile({ user, btnTitle }: AccountProfileProps) {
             </FormItem>
           )}
         />
-        <Button type='submit'>Submit</Button>
+        <Button type='submit' className='bg-primary-500'>
+          Submit
+        </Button>
       </form>
     </Form>
   );
