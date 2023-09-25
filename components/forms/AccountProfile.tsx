f05154f70de93471e405da8b6859f236895e8585
Karl George <karlgdev@gmail.com>
2023-09-25 15:10:35 +0200

    Get values from Clerk login

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -35,10 +35,10 @@ function AccountProfile({ user, btnTitle }: AccountProfileProps) {
   const form = useForm({
     resolver: zodResolver(userValidation),
     defaultValues: {
-      profile_photo: '',
-      name: '',
-      username: '',
-      bio: '',
+      profile_photo: user?.image || '',
+      name: user?.name || '',
+      username: user?.username || '',
+      bio: user?.bio || '',
     },
   });
 
