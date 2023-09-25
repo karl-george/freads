6742795bdf9ba4baaa5d4dad0dc91f11d18124d2
Karl George <karlgdev@gmail.com>
2023-09-25 12:20:15 +0200

    add logo and title to nav

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,9 +1,17 @@
-interface TopbarProps {
-  
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface TopbarProps {}
+
+function Topbar({}: TopbarProps) {
+  return (
+    <nav className='topbar'>
+      <Link href='/' className='flex items-center gap-4'>
+        <Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Freads</p>
+      </Link>
+    </nav>
+  );
 }
 
-function Topbar({}:TopbarProps) {
-  return <div>Topbar</div>
-}
-
-export default Topbar
+export default Topbar;
