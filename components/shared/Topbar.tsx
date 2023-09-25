7fee9f70382f593c06bdf74617f18ab3334fde88
Karl George <karlgdev@gmail.com>
2023-09-25 12:28:35 +0200

    adding Clerk signout and organisation switcher

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,15 +1,41 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { SignedIn, SignOutButton, OrganizationSwitcher } from '@clerk/nextjs';
 
 interface TopbarProps {}
 
 function Topbar({}: TopbarProps) {
   return (
     <nav className='topbar'>
+      {/* Logo */}
       <Link href='/' className='flex items-center gap-4'>
         <Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Freads</p>
       </Link>
+      {/* Clerk */}
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
+          <SignedIn>
+            <SignOutButton>
+              <div className='flex cursor-pointer'>
+                <Image
+                  src='/assets/logout.svg'
+                  alt='logout'
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: 'py-2 px-4',
+            },
+          }}
+        />
+      </div>
     </nav>
   );
 }
