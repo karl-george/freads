ce9db0dbd4572277e2687bf73de05cfb40c2f438
Karl George <karlgdev@gmail.com>
2023-09-26 14:08:49 +0200

    add thread actions

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,33 @@
+import { revalidatePath } from 'next/cache';
+import Thread from '../models/thread.model';
+import User from '../models/user.model';
+import { connectToDB } from '../mongoose';
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  connectToDB();
+
+  const createdThread = await Thread.create({
+    text,
+    author,
+    community: null,
+  });
+
+  // Update user model
+  await User.findByIdAndUpdate(author, {
+    $push: { threads: createdThread._id },
+  });
+
+  revalidatePath(path);
+}
