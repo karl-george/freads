98b6d38b2a4df4ed5f4c007c044c38786c5cccf3
Karl George <karlgdev@gmail.com>
2023-09-26 11:13:27 +0200

    add user model schema

---
+++ lib/models/user.model.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  threads: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thread' }],
+  onboarded: { type: Boolean, default: false },
+  communities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Community' }],
+});
+
+// First time mongoose.models will not exist so fall back to
+// create mongoose model user using the userSchema
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
