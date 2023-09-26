035df7481c8f32670de785a4e1c221a3698e9ec2
Karl George <karlgdev@gmail.com>
2023-09-26 16:22:49 +0200

    add displaying comments

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ThreadCard from '@/components/cards/ThreadCard';
+import Comment from '@/components/forms/Comment';
 import { fetchThreadById } from '@/lib/actions/thread.actions';
 import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
@@ -35,6 +36,29 @@ async function page({ params }: pageProps) {
           createdAt={thread.createdAt}
           comments={thread.children}
         />
+      </div>
+      <div className='mt-7'>
+        <Comment
+          threadId={thread._id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+      <div className='mt-10'>
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user?.id || ''}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment={true}
+          />
+        ))}
       </div>
     </section>
   );
