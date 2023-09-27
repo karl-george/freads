f434a8373ff8af34ab9b5e58b514d2be0f8d6278
Karl George <karlgdev@gmail.com>
2023-09-27 11:32:31 +0200

    add fetch specific users posts

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,48 @@
+import { fetchUserPosts } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
+import ThreadCard from '../cards/ThreadCard';
+
+interface ThreadsTabProps {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+async function ThreadsTab({
+  currentUserId,
+  accountId,
+  accountType,
+}: ThreadsTabProps) {
+  let result = await fetchUserPosts(accountId);
+  if (!result) {
+    redirect('/');
+  }
+
+  return (
+    <section className='mt-9 flex flex-col gap-10'>
+      {result.threads.map((thread: any) => (
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={currentUserId}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={
+            accountType === 'User'
+              ? { name: result.name, image: result.image, id: result.id }
+              : {
+                  name: thread.author.name,
+                  image: thread.author.image,
+                  id: thread.author.id,
+                }
+          }
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default ThreadsTab;
