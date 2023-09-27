9159e63c7851664646ac08f89d438b0bdeadb569
Karl George <karlgdev@gmail.com>
2023-09-27 14:16:21 +0200

    remove console log

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,8 +6,6 @@ export default async function Home() {
   const result = await fetchPosts(1, 30);
   const user = await currentUser();
 
-  console.log(result);
-
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
