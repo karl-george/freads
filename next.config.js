abf55ac12827ff71794c9552e2ba2baa81d1afce
Karl George <karlgdev@gmail.com>
2023-09-27 14:42:54 +0200

    add ignore ts errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ['mongoose'],
@@ -27,9 +30,6 @@ const nextConfig = {
         hostname: 'utfs.io',
       },
     ],
-    typescript: {
-      ignoreBuildErrors: true,
-    },
   },
 };
 
