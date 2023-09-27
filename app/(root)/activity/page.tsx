1714bfbca8dac04bb21cedce28ce78cd572e7e06
Karl George <karlgdev@gmail.com>
2023-09-27 12:33:50 +0200

    add activity page

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,11 @@
+interface pageProps {}
+
+function page({}: pageProps) {
+  return (
+    <section>
+      <h1 className='head-text mb-10'>Activity</h1>
+    </section>
+  );
+}
+
+export default page;
