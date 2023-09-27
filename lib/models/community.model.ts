49470782ccea3dc975429b33b4cc4b3ef07161fb
Karl George <karlgdev@gmail.com>
2023-09-27 14:43:18 +0200

    add community model

---
+++ lib/models/community.model.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const communitySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thread',
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+//  First time through models.user wont exist so fallback
+// to mongoose.model
+const Community =
+  mongoose.models.Community || mongoose.model('Community', communitySchema);
+
+export default Community;
